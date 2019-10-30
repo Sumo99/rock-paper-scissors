01fc4f491d13b453458657badbaa4f6a60f3113d
Sumo99 <36107758+Sumo99@users.noreply.github.com>
2019-10-29 21:00:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,24 +3,38 @@ let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
+const modifiers = ["Clone", "Cannibal", "Time Travel"]
 const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+const spock_div = document.getElementById("spock");
+const lizard_div = document.getElementById("lizard");
+const clone_modifer = document.getElementById("clone");
+const cannibal_modifer = document.getElementById("clone");
+const timetravel_modifer = document.getElementById("clone");
 
+function timeTravel() {
+
+}
+function modifier() {
+
+}
 function getComputerChoice() {
-    const choices = ['r', 'p', 's'];
-    const randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
+    const choices = ['r', 'p', 's', 'spock', 'lizard'];
+    const randomNumber = Math.floor(Math.random() * 5);
+    return "lizard";
 }
 
 function convertToWord(letter) {
     if (letter === "r") return "Rock";
     if (letter === "p") return "Paper";
+    if (letter === "spock") return "Spock";
+    if (letter === "lizard") return "Lizard"
     return "Scissors";
 }
 
-function win(userChoice, computerChoice) {
+function win(userChoice, computerChoice, ) {
     const smallUserWord = "user".fontsize(3).sup();
     const smallCompWord = "comp".fontsize(3).sup();
     const userChoice_div = document.getElementById(userChoice);
@@ -54,40 +68,64 @@ function draw(userChoice, computerChoice) {
 }
 
 function game(userChoice) {
+    //  debugger;
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice) {
         case "rs":
         case "pr":
         case "sp":
-        win(userChoice, computerChoice);
-        break;
+        case "lizardspock":
+        case "rlizard":
+        case "slizard":
+        case "lizardp":
+        case "pspock":
+        case "spockr":
+        case "spocks":
+            win(userChoice, computerChoice);
+            break;
         case "rp":
         case "ps":
         case "sr":
-        lose(userChoice, computerChoice);
-        break;
+        case "lizardr":
+        case "lizards":
+        case "plizard":
+        case "spocklizard":
+        case "spockp":
+        case "rspock":
+        case "sspock":
+            lose(userChoice, computerChoice);
+            break;
         case "rr":
         case "pp":
         case "ss":
-        draw(userChoice, computerChoice);
-        break;
+        case "spockspock":
+        case "lizardlizard":
+            draw(userChoice, computerChoice);
+            break;
 
     }
 }
 
 function main() {
 
-    rock_div.addEventListener('click', function() {
+    rock_div.addEventListener('click', function () {
         game("r");
     })
 
-    paper_div.addEventListener('click', function() {
+    paper_div.addEventListener('click', function () {
         game("p");
     })
 
-    scissors_div.addEventListener('click', function() {
+    scissors_div.addEventListener('click', function () {
         game("s");
     })
+    spock_div.addEventListener('click', function () {
+        game("spock");
+    })
+    lizard_div.addEventListener('click', function () {
+        game("lizard");
+    })
+
 }
 
 main();
