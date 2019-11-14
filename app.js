8aa14e778eee9f7eb0b7de87f248eb31142a8048
Sumo99 <36107758+Sumo99@users.noreply.github.com>
2019-11-13 21:17:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let userScore = 0;
 let computerScore = 0;
+let first_turn = true; //the markov chain has to choose randomly
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
@@ -11,19 +12,86 @@ const scissors_div = document.getElementById("s");
 const spock_div = document.getElementById("spock");
 const lizard_div = document.getElementById("lizard");
 const clone_modifer = document.getElementById("clone");
-const cannibal_modifer = document.getElementById("clone");
-const timetravel_modifer = document.getElementById("clone");
+const cannibal_modifer = document.getElementById("cannibal");
+const timetravel_modifer = document.getElementById("timetravel");
+const smallUserWord = "user".fontsize(3).sup();
+const smallCompWord = "comp".fontsize(3).sup();
+let lastUserChoice = "";
+let currentChoice = "";
+let markovArray = Array(5).fill().map(() => Array(5).fill(0));
+const rpsEnum = {
+    "r": 0,
+    "p": 1,
+    "s": 2,
+    "spock": 3,
+    "lizard": 4
+}
+function calculatemarkovProbability(choice) {
 
-function timeTravel() {
+    let v1 = markovArray[rpsEnum[choice]][0]; //these are the probabilities for a row of paper or other symbol
+    let v2 = markovArray[rpsEnum[choice]][1];
+    let v3 = markovArray[rpsEnum[choice]][2];
+    let v4 = markovArray[rpsEnum[choice]][3];
+    let v5 = markovArray[rpsEnum[choice]][4];
+
+    let p1 = v1 / (v1 + v2 + v3 + v4 + v5);
+    let p2 = v2 / (v1 + v2 + v3 + v4 + v5);
+    let p3 = v3 / (v1 + v2 + v3 + v4 + v5);
+    let p4 = v4 / (v1 + v2 + v3 + v4 + v5);
+    let p5 = v5 / (v1 + v2 + v3 + v4 + v5);
+
+    console.log(markovArray)
+    if (isNaN(p1))
+        p1 = 0;
+
+    if (isNaN(p2))
+        p2 = 0;
+
+    if (isNaN(p3))
+        p3 = 0;
+    if (isNaN(p4))
+        p4 = 0;
+    if (isNaN(p5))
+        p5 = 0;
+
+
+    // get the largest probability 
+    let largest = Math.max.apply(Math, [p1, p2, p3, p4, p5]);
+
+    // bot chooses rock
+    if (largest === p1)
+        return "r";
+
+    // bot chooses paper
+    else if (largest === p2)
+        return "p";
+
+    // bot chooses scissors
+    else if (largest === p3)
+        return "s";
+    else if (largest === p4)
+        return "spock";
+    else if (largest === p5)
+        return "lizard";
+}
+function timetravel() {
 
 }
-function modifier() {
+function clone() {
 
 }
+function cannibal() {
+    if (game(lastmoveplayer1) === 0) {
+        //user wins
+    }
+    lose()
+}
+
 function getComputerChoice() {
     const choices = ['r', 'p', 's', 'spock', 'lizard'];
     const randomNumber = Math.floor(Math.random() * 5);
-    return "lizard";
+    lastmoveplayer2 = choices[randomNumber];
+    return choices[randomNumber];
 }
 
 function convertToWord(letter) {
@@ -34,9 +102,7 @@ function convertToWord(letter) {
     return "Scissors";
 }
 
-function win(userChoice, computerChoice, ) {
-    const smallUserWord = "user".fontsize(3).sup();
-    const smallCompWord = "comp".fontsize(3).sup();
+function win(userChoice, computerChoice, score = 1) {
     const userChoice_div = document.getElementById(userChoice);
     userScore++;
     userScore_span.innerHTML = userScore;
@@ -46,9 +112,7 @@ function win(userChoice, computerChoice, ) {
     setTimeout(() => userChoice_div.classList.remove('green-glow'), 400);
 }
 
-function lose(userChoice, computerChoice) {
-    const smallUserWord = "user".fontsize(3).sup();
-    const smallCompWord = "comp".fontsize(3).sup();
+function lose(userChoice, computerChoice, score = -1) {
     const userChoice_div = document.getElementById(userChoice);
     computerScore++;
     userScore_span.innerHTML = userScore;
@@ -58,18 +122,19 @@ function lose(userChoice, computerChoice) {
     setTimeout(() => userChoice_div.classList.remove('red-glow'), 400);
 }
 
-function draw(userChoice, computerChoice) {
-    const smallUserWord = "user".fontsize(3).sup();
-    const smallCompWord = "comp".fontsize(3).sup();
+function draw(userChoice, computerChoice, score = 0) {
     const userChoice_div = document.getElementById(userChoice);
     result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord}.  It's a draw.`
     userChoice_div.classList.add('gray-glow');
     setTimeout(() => userChoice_div.classList.remove('gray-glow'), 400);
 }
 
-function game(userChoice) {
-    //  debugger;
-    const computerChoice = getComputerChoice();
+function choose_winner(userChoice, computerChoice) {
+    //const computerChoice = getComputerChoice();
+    if (userChoice === computerChoice) {
+        draw(userChoice, computerChoice);
+        return 0;
+    }
     switch (userChoice + computerChoice) {
         case "rs":
         case "pr":
@@ -82,28 +147,27 @@ function game(userChoice) {
         case "spockr":
         case "spocks":
             win(userChoice, computerChoice);
-            break;
-        case "rp":
-        case "ps":
-        case "sr":
-        case "lizardr":
-        case "lizards":
-        case "plizard":
-        case "spocklizard":
-        case "spockp":
-        case "rspock":
-        case "sspock":
+            return 1; //user won
+        default:
             lose(userChoice, computerChoice);
-            break;
-        case "rr":
-        case "pp":
-        case "ss":
-        case "spockspock":
-        case "lizardlizard":
-            draw(userChoice, computerChoice);
-            break;
-
+            return -1;
     }
+}
+function game(userChoice, modifiers = "") {
+    if (first_turn) {
+        const computerChoice = getComputerChoice();
+        first_turn = false;
+        lastUserChoice = userChoice;
+        currentChoice = userChoice;
+        choose_winner(currentChoice, computerChoice);
+    }
+    else {
+        lastUserChoice = currentChoice;
+        choose_winner(userChoice, calculatemarkovProbability(userChoice));
+        currentChoice = userChoice;
+        markovArray[rpsEnum[lastUserChoice]][rpsEnum[currentChoice]]++;
+    }
+
 }
 
 function main() {
